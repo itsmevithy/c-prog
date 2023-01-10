cc1a20ac2250d4619d5c53a5a1a74344c2b95828
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-01-10 11:35:54 +0530

    Update crypt.c

--- crypt.c
+++ crypt.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
 #include<string.h>
-void swapint(int a, int b);
-void swapstr(char a[100], char b[100]);
+void strrev(char *str){
+	int i, l;
+	char *start, *stop, ch;
+	l=strlen(str);
+	start=str;
+	stop=str+l-1;
+	for(i=0;i<(l-1)/2;i++){
+		ch=*stop;
+		*stop--=*start;
+		*start++=ch;
+	}
+}
+void strcopy(char *dest, char *source){
+	while (*src) *dest++= *src++;
+	*dest='\0';
+}
 void main(){
-	char code[5][100]={"a2"," mf4", "stupid3", "hes1"}, txt[5][100];
-	int num[5], i, j, a, c;
-	for(i=0; i<5; i++){
-		for(j=0; j<10; j++){
-			strassn(txt[i][j], code[i][j]);
-			if (code[i][j]=='\0'){
-				num[i]=code[j-1];
-				txt[i][j-1]='\0';}
-		}
+	int x, i, j, a, c;
+	printf("Enter number of words: ");
+	scanf("%d", &x);
+	char code[x][100], temp[100];
+	for (i=0; i<x; i++) {
+		gets(code[i]);
+		strrev(code[i]);
 	}
-	for(i=0; i<4; i++){
-		for(j=0 ; j<(4-i-1); j++ ){
-			if (num[j]>num[j+1])
-				swapint(num[j], num[j+1]);
-				swapstr(txt[j], txt[j+1]);
-		}
+	for (i=0; i<x; i++)
+		for(j=0; j<(x-i-1); j++)
+			if(code[j][0]>code[j+1][0]){
+				strcopy(temp, code[j]);
+				strcopy(code[j], code[j+1]);
+				strcopy(code[j+1], temp);
+			}
+	for(i=0; i<x; i++){
+		strrev(code[i]);
+		for(j=0; j<100; j++)
+			if(code[i][j]=='\0') { code[i][j-1]='\0'; break; }
+		puts(code[i]);
 	}
-	for(i=0; i<5; i++) puts(txt[i]);
-}
-
-void swapint(int a, int b){
-	int temp;
-	temp=a;
-	a=b;
-	b=temp;
-}
-
-void swapstr(char a[100], char b[100]){
-	char temp[100];
-	strcpy(temp, a);
-	strcpy(a, b);
-	strcpy(b, temp);
 }
